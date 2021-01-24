9154795f79d1f47e0a7830f976ec6c0e5a3ed365
Wouter Hovius <info@wingingit.nl>
2021-01-24 15:28:04 +0100

    first version. ToDo: - add in wow.js animation. - Explore other display options. - Add adlocker detection.

---
+++ resources/js/main.js
@@ -0,0 +1,12 @@
+// Laravel Application Bootstrap
+require('./bootstrap');
+
+// Bootstrap 5: https://getbootstrap.com/docs/5.0
+require('bootstrap/dist/js/bootstrap');
+require('wow.js/dist/wow.min');
+require('tiny-slider/src/tiny-slider');
+require('glightbox/src/js/glightbox');
+require('@fortawesome/fontawesome-free/js/all');
+
+
+new WOW().init();
