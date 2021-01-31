88a8a315289681db6cfbcb339a9d5555fe8d0689
Wouter Hovius <info@wingingit.nl>
2021-01-31 14:23:56 +0100

    adjust sprite loading

--- resources/views/partials/players.blade.php
+++ resources/views/partials/players.blade.php
@@ -1,8 +1,8 @@
 <div class="row p-4">
     @foreach(config('team.names') as $teammember)
         <div class="col-4 col-md-2 zoomIn">
-            @if(file_exists(public_path('images/players/'. $teammember . '.gif')))
-                <img class="img-fluid img wow"  data-bs-toggle="tooltip" title="{{$teammember}}" src="{{asset('images/players/'. $teammember . '.gif')}}"/>
+            @if(file_exists(public_path(\Illuminate\Support\Str::lower('images/players/'. $teammember . '.gif'))))
+                <img class="img-fluid img wow"  data-bs-toggle="tooltip" title="{{$teammember}}" src="{{asset(\Illuminate\Support\Str::lower('images/players/'. $teammember . '.gif'))}}"/>
             @else
                 <img class="img-fluid img wow"  data-bs-toggle="tooltip" title="{{$teammember}}" src="{{asset('images/players/template.gif')}}"/>
             @endif
